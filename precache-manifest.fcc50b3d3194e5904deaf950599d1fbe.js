ea947e9c1fda203991e1891af4f4bae8d4ade8e9
Nez <checkman3335@gmail.com>
2020-05-08 17:36:52 -0700

    Updates

---
+++ precache-manifest.fcc50b3d3194e5904deaf950599d1fbe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e7728aff1853c68de53bd9022ab374",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "997242dadb72008ab6e2",
+    "url": "/game-of-life/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ac9de7c5cc1427552eeb",
+    "url": "/game-of-life/static/css/main.2f3712f9.chunk.css"
+  },
+  {
+    "revision": "997242dadb72008ab6e2",
+    "url": "/game-of-life/static/js/2.0d784d91.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/game-of-life/static/js/2.0d784d91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac9de7c5cc1427552eeb",
+    "url": "/game-of-life/static/js/main.63052001.chunk.js"
+  },
+  {
+    "revision": "e1984ba9369d1f63cc07",
+    "url": "/game-of-life/static/js/runtime-main.395d4fc5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/game-of-life/static/media/logo.5d5d9eef.svg"
+  }
+]);
